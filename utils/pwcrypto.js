9658884402b3b0a47b2f3d8c34febf6378ed5a2c
13circle <13circle97@gmail.com>
2020-11-19 23:11:37 +0900

    Add password encryption module

---
+++ utils/pwcrypto.js
@@ -0,0 +1,35 @@
+const crypto = require("crypto");
+
+require("dotenv").config(require("@config/dotenvConfig"));
+
+function encrypt(text) {
+  let iv = crypto.randomBytes(IV_LENGTH);
+  let cipher = crypto.createCipheriv(
+    CRYPTO_ALGORITHM,
+    Buffer.from(ENCRYPTION_KEY),
+    iv
+  );
+  let encrypted = cipher.update(text);
+
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+  return iv.toString("hex") + ":" + encrypted.toString("hex");
+}
+
+function decrypt(text) {
+  let textParts = text.split(":");
+  let iv = Buffer.from(textParts.shift(), "hex");
+  let encryptedText = Buffer.from(textParts.join(":"), "hex");
+  let decipher = crypto.createDecipheriv(
+    CRYPTO_ALGORITHM,
+    Buffer.from(ENCRYPTION_KEY),
+    iv
+  );
+  let decrypted = decipher.update(encryptedText);
+
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+  return decrypted.toString();
+}
+
+module.exports = { encrypt, decrypt };
